e582e311fd78edbaf529c471f3cec2aa53363b76
Aleksandr Seliukov <asexsela@gmail.com>
2023-01-02 14:33:07 +0100

    fix

--- src/configs/mongo.config.ts
+++ src/configs/mongo.config.ts
@@ -1,12 +1,12 @@
-import { ConfigService } from '@nestjs/config'
-import { TypegooseModuleOptions } from 'nestjs-typegoose'
+import { ConfigService } from '@nestjs/config';
+import { TypegooseModuleOptions } from 'nestjs-typegoose';
 
 export const getMongoConfig = async (configService: ConfigService): Promise<TypegooseModuleOptions> => {
 	return {
 		uri: getMongoString(configService),
 		...getMongoOptions()
-	}
-}
+	};
+};
 
 const getMongoString = (configService: ConfigService) =>
 	'mongodb://'+
@@ -18,9 +18,9 @@ const getMongoString = (configService: ConfigService) =>
 	':'+
 	configService.get('MONGO_PORT')+
 	'/'+
-	configService.get('MONGO_AUTHDATABASE')
+	configService.get('MONGO_AUTHDATABASE');
 
 const getMongoOptions = () => ({
 	useNewUrlParser: true,
 	useUnifiedTopology: true
-})
+});
