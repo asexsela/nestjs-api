595f93533c6c0decb5e8e16234df7514e94dd756
Aleksandr Seliukov <asexsela@gmail.com>
2023-01-03 12:10:44 +0100

    add telegram module

--- src/app.module.ts
+++ src/app.module.ts
@@ -10,21 +10,30 @@ import { TypegooseModule } from 'nestjs-typegoose';
 import { getMongoConfig } from './configs/mongo.config';
 import { FilesModule } from './files/files.module';
 import { SitemapModule } from './sitemap/sitemap.module';
+import { TelegramModule } from './telegram/telegram.module';
+import { getTelegramConfig } from './configs/telegram.config';
 
 @Module({
-  imports: [
-	ConfigModule.forRoot(),
-	TypegooseModule.forRootAsync({
-		imports: [ConfigModule],
-		inject: [ConfigService],
-		useFactory: getMongoConfig
-	}),
-	AuthModule, 
-	TopPageModule, 
-	ProductModule, 
-	ReviewModule, FilesModule, SitemapModule
-  ],
-  controllers: [AppController],
-  providers: [AppService],
+	imports: [
+		ConfigModule.forRoot(),
+		TypegooseModule.forRootAsync({
+			imports: [ConfigModule],
+			inject: [ConfigService],
+			useFactory: getMongoConfig
+		}),
+		AuthModule,
+		TopPageModule,
+		ProductModule,
+		ReviewModule,
+		FilesModule,
+		SitemapModule,
+		TelegramModule.forRootAsync({
+			imports: [ConfigModule],
+			inject: [ConfigService],
+			useFactory: getTelegramConfig
+		})
+	],
+	controllers: [AppController],
+	providers: [AppService],
 })
 export class AppModule {}
--- src/review/review.controller.ts
+++ src/review/review.controller.ts
@@ -5,15 +5,31 @@ import { UserEmail } from '../decorators/user-email.decorator';
 import { CreateReviewDto } from './dto/create-review.dto';
 import { REVIEW_NOT_FOUND } from './review.constants';
 import { ReviewService } from './review.service';
+import { TelegramService } from '../telegram/telegram.service';
 
 @Controller('review')
 export class ReviewController {
-	constructor(private readonly reviewService: ReviewService) {}
+	constructor(
+		private readonly reviewService: ReviewService,
+		private readonly telegramService: TelegramService
+	) {}
 
 	@UsePipes(new ValidationPipe())
 	@Post('create')
 	async create(@Body() dto: CreateReviewDto) {
 		return this.reviewService.create(dto);
+	}
+	
+	@UsePipes(new ValidationPipe())
+	@Post('notify')
+	async notify(@Body() dto: CreateReviewDto) {
+		const message = `Name: ${dto.name}\n`
+			+ `Title: ${dto.title}\n`
+			+ `Desc: ${dto.description}\n`
+			+ `Rating: ${dto.rating}\n`
+			+ `Product ID: ${dto.productId}`;
+		
+		return this.telegramService.sendMessage(message);
 	}
 
 	@UseGuards(JwtAuthGuard)
@@ -28,7 +44,7 @@ export class ReviewController {
 
 	// @UseGuards(JwtAuthGuard)
 	@Get('byProduct/:productId')
-	async getByProduct(@Param('productId', IdValidationPipe) productId: string, @UserEmail() email: string) {		
+	async getByProduct(@Param('productId', IdValidationPipe) productId: string, @UserEmail() email: string) {
 		return this.reviewService.findByProductId(productId);
 	}
 }
